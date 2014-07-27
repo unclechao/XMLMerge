1d613e49f47ca394036370cbe2240639c9b87965
Uncle.Chao <uncle.chao@live.cn>
2014-07-27 16:15:55 +0800

    Main Class

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XMLUpdateMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string oldXMLPath = @"G:\My Projects\Old.xml";
+            string newXMLPath = @"G:\My Projects\New.xml";
+
+            XMLMerge mergeTool = new XMLMerge();
+            mergeTool.UpdateXMLFile(oldXMLPath, newXMLPath);
+            Console.WriteLine("XML Merge finished! Press ant key to exit");
+            Console.ReadKey();
+        }
+    }
+}
